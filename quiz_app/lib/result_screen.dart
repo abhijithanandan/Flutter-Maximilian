1068378ea6c21f116bf72c8e2373abaa7831d07e
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-15 23:34:59 +0530

    Added a function to return the necessary data to populate the answer display widgets in result screen

--- quiz_app/lib/result_screen.dart
+++ quiz_app/lib/result_screen.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/data/questions.dart';
 
 class ResultScreen extends StatelessWidget {
   const ResultScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summaryData = [];
+    for (int i = 0; i < chosenAnswers.length; i++) {
+      summaryData.add({
+        'question_index': i,
+        'question': questions[i].text,
+        'correct_answer': questions[i].answers[0],
+        'user_answer': chosenAnswers[i],
+      });
+    }
+    return summaryData;
+  }
 
   @override
   Widget build(BuildContext context) {

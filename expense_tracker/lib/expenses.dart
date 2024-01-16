7ff09825a7b2b8a1f763daa3ab228261ed1a4758
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-16 15:51:59 +0530

    Added the ExpensesTrackerApp widget that will be the app parent widget

---
+++ expense_tracker/lib/expenses.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class ExpensesTrackerApp extends StatefulWidget {
+  const ExpensesTrackerApp({super.key});
+
+  @override
+  State<ExpensesTrackerApp> createState() => _ExpensesTrackerAppState();
+}
+
+class _ExpensesTrackerAppState extends State<ExpensesTrackerApp> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Expenses Tracker',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Text('Hello World!'),
+    );
+  }
+}

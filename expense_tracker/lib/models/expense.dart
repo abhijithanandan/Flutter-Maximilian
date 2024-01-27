4fda8952b7635c267acbd7873e10bc1ee46555b1
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-28 00:44:58 +0530

    Added uuid to expense class

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,13 +1,14 @@
+import 'package:uuid/uuid.dart';
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
 
-  const Expense({
-    required this.id,
+  Expense({
     required this.title,
     required this.amount,
     required this.date,
-  });
+  }) : id = const Uuid().v4();
 }

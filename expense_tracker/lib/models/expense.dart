7ab14e7984d9db256b6f34bc079e673c808eddde
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-28 00:52:23 +0530

    Added the Category enum to the expenses class

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,12 +1,16 @@
 import 'package:uuid/uuid.dart';
 
+enum Category { food, travel, leisure, work }
+
 class Expense {
   final String id;
   final String title;
   final double amount;
   final DateTime date;
+  final Category category;
 
   Expense({
+    required this.category,
     required this.title,
     required this.amount,
     required this.date,

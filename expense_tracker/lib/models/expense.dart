e16d627d5888bcd973d93ff643e2191bc9295392
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-28 14:45:45 +0530

    Added category icons map to the expense class file

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -1,6 +1,14 @@
+import 'package:flutter/material.dart';
 import 'package:uuid/uuid.dart';
 
 enum Category { food, travel, leisure, work }
+
+const categoryIcons = {
+  Category.food: Icons.lunch_dining,
+  Category.travel: Icons.flight_takeoff,
+  Category.leisure: Icons.movie,
+  Category.work: Icons.work,
+};
 
 class Expense {
   final String id;

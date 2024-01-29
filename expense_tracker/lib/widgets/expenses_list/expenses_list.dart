3dd1c46d1f2ddd1dac0c9c6780be86f6511b2ea7
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-30 00:18:03 +0530

    Added a background for the Dismissible widget to show while swiped

--- expense_tracker/lib/widgets/expenses_list/expenses_list.dart
+++ expense_tracker/lib/widgets/expenses_list/expenses_list.dart
@@ -16,6 +16,12 @@ class ExpenseList extends StatelessWidget {
       itemBuilder: (context, index) {
         return Dismissible(
           key: ValueKey(expenses[index]),
+          background: Container(
+            color: Theme.of(context).colorScheme.error,
+            margin: EdgeInsets.symmetric(
+              horizontal: Theme.of(context).cardTheme.margin!.horizontal,
+            ),
+          ),
           onDismissed: (direction) => onRemoveExpense(expenses[index]),
           child: ExpenseItem(expenses[index]),
         );

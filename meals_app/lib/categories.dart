1e34ff2f8b8e136e307e1bd6f0f54b2a45bd6ae0
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-31 05:01:24 +0530

    Added the CategoryGridItems to the  CategoriesScreen widget

--- meals_app/lib/categories.dart
+++ meals_app/lib/categories.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
@@ -16,7 +18,10 @@ class CategoriesScreen extends StatelessWidget {
           childAspectRatio: 3 / 2,
           mainAxisSpacing: 20,
         ),
-        children: [],
+        children: [
+          for (final category in availableCategories)
+            CategoryGridItem(category: category)
+        ],
       ),
     );
   }

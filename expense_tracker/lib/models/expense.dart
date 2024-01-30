7528dda3bf4670387a26563896cb43a270d24529
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-30 12:06:59 +0530

    Added the chart bucket class

--- expense_tracker/lib/models/expense.dart
+++ expense_tracker/lib/models/expense.dart
@@ -29,3 +29,29 @@ class Expense {
     required this.date,
   }) : id = const Uuid().v4();
 }
+
+class ExpenseBucket {
+  final Category category;
+  final List<Expense> expenses;
+
+  const ExpenseBucket({
+    required this.category,
+    required this.expenses,
+  });
+
+  /// Add only expnses that are of the same category while creating a new bucket
+  ExpenseBucket.forCategory(List<Expense> allExpenses, this.category)
+      : expenses = allExpenses
+            .where((expense) => expense.category == category)
+            .toList();
+
+  double get totalExpense {
+    double sum = 0;
+
+    for (final expense in expenses) {
+      sum += expense.amount;
+    }
+
+    return sum;
+  }
+}

b971fc2ea6c2832b5d3f51bf0f5e8f256e53e6d1
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-30 13:26:12 +0530

    Locked the app to only have portraitUp orientation

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:expense_tracker/expenses.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter/material.dart';
 
 var kColorScheme =
@@ -8,63 +9,67 @@ var kDarkColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 8, 7, 7));
 
 void main() {
-  runApp(
-    MaterialApp(
-      darkTheme: ThemeData.dark().copyWith(
-        colorScheme: kDarkColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kDarkColorScheme.onPrimaryContainer,
-          foregroundColor: kDarkColorScheme.onPrimary,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kDarkColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 8.0,
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp,
+  ]).then(
+    (fn) => runApp(
+      MaterialApp(
+        darkTheme: ThemeData.dark().copyWith(
+          colorScheme: kDarkColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kDarkColorScheme.onPrimaryContainer,
+            foregroundColor: kDarkColorScheme.onPrimary,
           ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kDarkColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData.dark().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.normal,
-                color: kDarkColorScheme.onSecondaryContainer,
-              ),
+          cardTheme: const CardTheme().copyWith(
+            color: kDarkColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
             ),
-      ),
-      themeMode: ThemeMode.system,
-      theme: ThemeData().copyWith(
-        colorScheme: kColorScheme,
-        appBarTheme: const AppBarTheme().copyWith(
-          backgroundColor: kColorScheme.onPrimaryContainer,
-          foregroundColor: kColorScheme.onPrimary,
-        ),
-        cardTheme: const CardTheme().copyWith(
-          color: kColorScheme.secondaryContainer,
-          margin: const EdgeInsets.symmetric(
-            horizontal: 16.0,
-            vertical: 8.0,
           ),
-        ),
-        elevatedButtonTheme: ElevatedButtonThemeData(
-          style: ElevatedButton.styleFrom(
-            backgroundColor: kColorScheme.primaryContainer,
-          ),
-        ),
-        textTheme: ThemeData().textTheme.copyWith(
-              titleLarge: TextStyle(
-                fontSize: 14.0,
-                fontWeight: FontWeight.normal,
-                color: kColorScheme.onSecondaryContainer,
-              ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kDarkColorScheme.primaryContainer,
             ),
+          ),
+          textTheme: ThemeData.dark().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.normal,
+                  color: kDarkColorScheme.onSecondaryContainer,
+                ),
+              ),
+        ),
+        themeMode: ThemeMode.system,
+        theme: ThemeData().copyWith(
+          colorScheme: kColorScheme,
+          appBarTheme: const AppBarTheme().copyWith(
+            backgroundColor: kColorScheme.onPrimaryContainer,
+            foregroundColor: kColorScheme.onPrimary,
+          ),
+          cardTheme: const CardTheme().copyWith(
+            color: kColorScheme.secondaryContainer,
+            margin: const EdgeInsets.symmetric(
+              horizontal: 16.0,
+              vertical: 8.0,
+            ),
+          ),
+          elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+              backgroundColor: kColorScheme.primaryContainer,
+            ),
+          ),
+          textTheme: ThemeData().textTheme.copyWith(
+                titleLarge: TextStyle(
+                  fontSize: 14.0,
+                  fontWeight: FontWeight.normal,
+                  color: kColorScheme.onSecondaryContainer,
+                ),
+              ),
+        ),
+        debugShowCheckedModeBanner: false, // Remove debug banner
+        home: const ExpensesTrackerApp(),
       ),
-      debugShowCheckedModeBanner: false, // Remove debug banner
-      home: const ExpensesTrackerApp(),
     ),
   );
 }

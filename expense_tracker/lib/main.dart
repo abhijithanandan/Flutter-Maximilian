21560b2ce49c582c1d25a3193ef2706713596dfb
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-01-30 09:02:31 +0530

    Added a dark color scheme

--- expense_tracker/lib/main.dart
+++ expense_tracker/lib/main.dart
@@ -4,9 +4,39 @@ import 'package:flutter/material.dart';
 var kColorScheme =
     ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 111, 27, 179));
 
+var kDarkColorScheme =
+    ColorScheme.fromSeed(seedColor: const Color.fromARGB(255, 210, 232, 223));
+
 void main() {
   runApp(
     MaterialApp(
+      darkTheme: ThemeData.dark().copyWith(
+        colorScheme: kDarkColorScheme,
+        appBarTheme: const AppBarTheme().copyWith(
+          backgroundColor: kDarkColorScheme.onPrimaryContainer,
+          foregroundColor: kDarkColorScheme.onPrimaryContainer,
+        ),
+        cardTheme: const CardTheme().copyWith(
+          color: kDarkColorScheme.secondaryContainer,
+          margin: const EdgeInsets.symmetric(
+            horizontal: 16.0,
+            vertical: 8.0,
+          ),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: kDarkColorScheme.primaryContainer,
+          ),
+        ),
+        textTheme: ThemeData.dark().textTheme.copyWith(
+              titleLarge: TextStyle(
+                fontSize: 14.0,
+                fontWeight: FontWeight.normal,
+                color: kDarkColorScheme.onSecondaryContainer,
+              ),
+            ),
+      ),
+      themeMode: ThemeMode.system,
       theme: ThemeData().copyWith(
         colorScheme: kColorScheme,
         appBarTheme: const AppBarTheme().copyWith(

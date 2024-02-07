93281b88bac00a170d340559f967cd260506178a
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-07 14:35:56 +0530

    Updated the page route to pass actual meals data

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/models/category.dart';
 import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
 
-  void _selectCategory(BuildContext context) {
+  void _selectCategory(BuildContext context, Category category) {
+    final filteredMeals = dummyMeals.where((meal) {
+      return meal.categories.contains(category.id);
+    }).toList();
+
     Navigator.push(
       context,
       MaterialPageRoute(
-        builder: (cxt) => const MealsScreen(
-          title: 'Meals',
-          meals: [],
+        builder: (cxt) => MealsScreen(
+          title: category.title,
+          meals: filteredMeals,
         ),
       ),
     );
@@ -37,7 +42,7 @@ class CategoriesScreen extends StatelessWidget {
           for (final category in availableCategories)
             CategoryGridItem(
               category: category,
-              selectCategory: () => _selectCategory(context),
+              selectCategory: () => _selectCategory(context, category),
             ),
         ],
       ),

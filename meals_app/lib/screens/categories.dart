a640b5b2a315bd62314abfe7c9a5eba4dfa5fe13
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-07 14:18:21 +0530

    Added navigation using the Navigator widget provided by flutter

--- meals_app/lib/screens/categories.dart
+++ meals_app/lib/screens/categories.dart
@@ -1,9 +1,22 @@
 import 'package:flutter/material.dart';
 import 'package:meals_app/data/dummy_data.dart';
+import 'package:meals_app/screens/meals.dart';
 import 'package:meals_app/widgets/category_grid_item.dart';
 
 class CategoriesScreen extends StatelessWidget {
   const CategoriesScreen({super.key});
+
+  void _selectCategory(BuildContext context) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (cxt) => const MealsScreen(
+          title: 'Meals',
+          meals: [],
+        ),
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,7 +35,10 @@ class CategoriesScreen extends StatelessWidget {
         children: [
           // availableCategories.map((category) => CategoryGridItem(category: category)).toList(),
           for (final category in availableCategories)
-            CategoryGridItem(category: category)
+            CategoryGridItem(
+              category: category,
+              selectCategory: () => _selectCategory(context),
+            ),
         ],
       ),
     );

dceb649745c1ed08d684afb0f20bd97eaefe855a
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-08 14:11:54 +0530

    Commented out the drawer on filter screen

--- meals_app/lib/screens/filters_screen.dart
+++ meals_app/lib/screens/filters_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:meals_app/screens/tabs.dart';
-import 'package:meals_app/widgets/main_drawer.dart';
+// import 'package:meals_app/screens/tabs.dart';
+// import 'package:meals_app/widgets/main_drawer.dart';
 
 class FiltersScreen extends StatefulWidget {
   const FiltersScreen({super.key});
@@ -18,16 +18,16 @@ class _FiltersScreenState extends State<FiltersScreen> {
       appBar: AppBar(
         title: const Text('Filters'),
       ),
-      drawer: MainDrawer(onSelectScreen: (identifier) {
-        Navigator.of(context).pop();
-        if (identifier == 'meals') {
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(
-              builder: (ctx) => const TabsScreen(),
-            ),
-          );
-        }
-      }),
+      // drawer: MainDrawer(onSelectScreen: (identifier) {
+      //   Navigator.of(context).pop();
+      //   if (identifier == 'meals') {
+      //     Navigator.of(context).pushReplacement(
+      //       MaterialPageRoute(
+      //         builder: (ctx) => const TabsScreen(),
+      //       ),
+      //     );
+      //   }
+      // }),
       body: Column(
         children: [
           SwitchListTile(

8599c9b3400f01f2ad270a8c41a48c002c43fed6
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-13 09:21:00 +0530

    Added the basic files and widgets for places

---
+++ favorite_places/lib/screens/places.dart
@@ -0,0 +1,26 @@
+import 'package:favorite_places/widgets/places_list.dart';
+import 'package:flutter/material.dart';
+
+class PlacesScreen extends StatelessWidget {
+  const PlacesScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Your Places'),
+        actions: <Widget>[
+          IconButton(
+            icon: const Icon(Icons.add),
+            onPressed: () {
+              // Navigate to AddPlaceScreen
+            },
+          ),
+        ],
+      ),
+      body: const PlacesList(
+        places: [],
+      ),
+    );
+  }
+}

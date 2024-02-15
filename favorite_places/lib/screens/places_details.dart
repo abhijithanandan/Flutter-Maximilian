f961f1b4037297bec0207e48c90b056fc8264beb
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-15 14:39:53 +0530

    Added image to places details screen

--- favorite_places/lib/screens/places_details.dart
+++ favorite_places/lib/screens/places_details.dart
@@ -11,11 +11,15 @@ class PlaceDetailsScreen extends StatelessWidget {
     return Scaffold(
       appBar: AppBar(title: Text(place.title)),
       body: Center(
-        child: Text(
-          place.title,
-          style: Theme.of(context).textTheme.titleMedium!.copyWith(
-                color: Theme.of(context).colorScheme.onBackground,
-              ),
+        child: Stack(
+          children: [
+            Image.file(
+              place.image,
+              fit: BoxFit.cover,
+              width: double.infinity,
+              height: double.infinity,
+            )
+          ],
         ),
       ),
     );

6c190ed846b5f4cc740a0deae9c2ea1b91e96e4b
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-17 18:13:40 +0530

    Added the address to places list and places detail screen

--- favorite_places/lib/screens/places_details.dart
+++ favorite_places/lib/screens/places_details.dart
@@ -18,7 +18,28 @@ class PlaceDetailsScreen extends StatelessWidget {
               fit: BoxFit.cover,
               width: double.infinity,
               height: double.infinity,
-            )
+            ),
+            Positioned(
+              bottom: 0,
+              right: 0,
+              left: 0,
+              child: Column(
+                children: [
+                  CircleAvatar(
+                    radius: 26,
+                    backgroundImage: FileImage(
+                      place.image,
+                    ),
+                  ),
+                  Text(
+                    place.location.address,
+                    style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onBackground,
+                        ),
+                  ),
+                ],
+              ),
+            ),
           ],
         ),
       ),

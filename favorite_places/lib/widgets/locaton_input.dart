5ad1126a3de789c937585a44e3c151629f4ab65b
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-17 11:06:13 +0530

    Retreved the address from google maps geolocation api

--- favorite_places/lib/widgets/locaton_input.dart
+++ favorite_places/lib/widgets/locaton_input.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
 import 'dart:developer';
 
 import 'package:flutter/material.dart';
 import 'package:location/location.dart';
+import 'package:http/http.dart' as http;
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class LocationInput extends StatefulWidget {
   const LocationInput({super.key});
@@ -41,6 +44,15 @@ class _LocationInputState extends State<LocationInput> {
       _isGettingLocation = true;
     });
     locationData = await location.getLocation();
+    final lat = locationData.latitude;
+    final lng = locationData.longitude;
+    final url = Uri.parse(
+      'https://maps.googleapis.com/maps/api/geocode/json?latlng=$lat,$lng&key=${dotenv.env['GOOGLE_API_KEY']}',
+    );
+
+    final response = await http.get(url);
+    final resData = json.decode(response.body);
+    final address = resData['results'][0]['formatted_address'];
 
     setState(() {
       _isGettingLocation = false;

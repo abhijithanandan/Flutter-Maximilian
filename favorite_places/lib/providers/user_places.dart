46b51bb8af711c4b8ffe4adde1cb64a58b3f328f
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-18 09:15:04 +0530

    Wrote function to load the list of places from local storage

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -1,14 +1,45 @@
 import 'dart:io';
 
 import 'package:favorite_places/models/place.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
 import 'package:sqflite/sqflite.dart' as sqflite;
 import 'package:sqflite/sqlite_api.dart';
 
+Future<sqflite.Database> _getDatabase() async {
+  final dbPath = await sqflite.getDatabasesPath();
+  final db = await sqflite.openDatabase(path.join(dbPath, 'places.db'),
+      onCreate: (db, version) {
+    return db.execute(
+        'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)');
+  }, version: 1);
+  return db;
+}
+
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
+
+  void loadPlaces() async {
+    sqflite.Database db = await _getDatabase();
+    final data = await db.query('user_places');
+    final places = data
+        .map(
+          (row) => Place(
+            title: row['title'] as String,
+            image: File(row['image'] as String),
+            location: PlaceLocation(
+              latitude: row['loc_lat'] as double,
+              longitude: row['loc_lng'] as double,
+              address: row['address'] as String,
+            ),
+            id: row['id'] as String,
+          ),
+        )
+        .toList();
+    state = places;
+  }
 
   void addPlace(String title, File image, PlaceLocation location) async {
     final appDir = await syspaths.getApplicationDocumentsDirectory();
@@ -17,12 +48,7 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
 
     final newPlace = Place(title: title, image: savedImage, location: location);
 
-    final dbPath = await sqflite.getDatabasesPath();
-    final db = await sqflite.openDatabase(path.join(dbPath, 'places.db'),
-        onCreate: (db, version) {
-      return db.execute(
-          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)');
-    }, version: 1);
+    sqflite.Database db = await _getDatabase();
 
     db.insert('user_places', {
       'id': newPlace.id,

a5a0ad98084a83c35c4ef0a849582db2cb255df8
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-18 08:35:33 +0530

    Saved the image in local directory in the device

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -2,12 +2,18 @@ import 'dart:io';
 
 import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:path_provider/path_provider.dart' as syspaths;
+import 'package:path/path.dart' as path;
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
 
-  void addPlace(String title, File image, PlaceLocation location) {
-    final newPlace = Place(title: title, image: image, location: location);
+  void addPlace(String title, File image, PlaceLocation location) async {
+    final appDir = await syspaths.getApplicationDocumentsDirectory();
+    final filename = path.basename(image.path);
+    final savedImage = await image.copy('${appDir.path}/$filename');
+
+    final newPlace = Place(title: title, image: savedImage, location: location);
     state = [...state, newPlace];
   }
 }

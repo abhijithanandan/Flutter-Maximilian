bb230ae62541158ecdbdf30e48e7827d2cc9cefb
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-18 08:54:07 +0530

    Stored the metadata to the local sqflite database

--- favorite_places/lib/providers/user_places.dart
+++ favorite_places/lib/providers/user_places.dart
@@ -4,6 +4,8 @@ import 'package:favorite_places/models/place.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:path_provider/path_provider.dart' as syspaths;
 import 'package:path/path.dart' as path;
+import 'package:sqflite/sqflite.dart' as sqflite;
+import 'package:sqflite/sqlite_api.dart';
 
 class UserPlacesNotifier extends StateNotifier<List<Place>> {
   UserPlacesNotifier() : super([]);
@@ -14,6 +16,23 @@ class UserPlacesNotifier extends StateNotifier<List<Place>> {
     final savedImage = await image.copy('${appDir.path}/$filename');
 
     final newPlace = Place(title: title, image: savedImage, location: location);
+
+    final dbPath = await sqflite.getDatabasesPath();
+    final db = await sqflite.openDatabase(path.join(dbPath, 'places.db'),
+        onCreate: (db, version) {
+      return db.execute(
+          'CREATE TABLE user_places(id TEXT PRIMARY KEY, title TEXT, image TEXT, loc_lat REAL, loc_lng REAL, address TEXT)');
+    }, version: 1);
+
+    db.insert('user_places', {
+      'id': newPlace.id,
+      'title': newPlace.title,
+      'image': newPlace.image.path,
+      'loc_lat': newPlace.location.latitude,
+      'loc_lng': newPlace.location.longitude,
+      'address': newPlace.location.address,
+    });
+
     state = [...state, newPlace];
   }
 }

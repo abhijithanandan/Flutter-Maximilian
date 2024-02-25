1867f57aa01e3b913ee74bd8f028ee603e9216b7
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-26 03:36:27 +0530

    Added StreamBuilder to listen and display the chats

--- chat_app/lib/widgets/chat_message.dart
+++ chat_app/lib/widgets/chat_message.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class ChatMessage extends StatelessWidget {
@@ -5,8 +6,32 @@ class ChatMessage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('No messages found...'),
+    return StreamBuilder(
+      stream: FirebaseFirestore.instance
+          .collection('chat')
+          .orderBy('createdAt', descending: false)
+          .snapshots(),
+      builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return const Center(
+            child: CircularProgressIndicator(),
+          );
+        }
+
+        if (!snapshot.hasData || snapshot.data!.docs.isEmpty) {
+          return const Center(
+            child: Text('No messages yet'),
+          );
+        }
+
+        final loadedMessages = snapshot.data!.docs;
+        return ListView.builder(
+          itemCount: loadedMessages.length,
+          itemBuilder: (context, index) => ListTile(
+            title: Text(loadedMessages[index].data()['text']),
+          ),
+        );
+      },
     );
   }
 }

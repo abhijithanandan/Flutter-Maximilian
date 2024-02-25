0085d55210fa8d323008da7dd9596e0edda3c4a3
Abhijith Anandakrishnan <anandakrishnanabhijith@gmail.com>
2024-02-25 15:34:10 +0530

    Added user auth with email and password using FirebaseAuth

--- chat_app/lib/screens/auth.dart
+++ chat_app/lib/screens/auth.dart
@@ -1,4 +1,7 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+
+final _firebase = FirebaseAuth.instance;
 
 class AuthScreen extends StatefulWidget {
   const AuthScreen({super.key});
@@ -13,11 +16,32 @@ class _AuthScreenState extends State<AuthScreen> {
   var _enteredEmail = '';
   var _enteredPassword = '';
 
-  void _submit() {
+  void _submit() async {
     final isValid = _form.currentState!.validate();
 
     if (!isValid) {
       _form.currentState!.save();
+      if (_isLogin) {
+        // Log user in
+      } else {
+        try {
+          final UserCredential = await _firebase.createUserWithEmailAndPassword(
+            email: _enteredEmail,
+            password: _enteredPassword,
+          );
+        } on FirebaseAuthException catch (error) {
+          if (error.code == 'email-already-in-use') {}
+          ScaffoldMessenger.of(context).clearSnackBars();
+          ScaffoldMessenger.of(context).showSnackBar(
+            SnackBar(
+              content: Text(error.message ?? 'An error occurred'),
+              backgroundColor: Theme.of(context).colorScheme.error,
+            ),
+          );
+        }
+      }
+    } else {
+      return;
     }
   }
 
